e8fe6cb91120d7a3cb4ac3da2e655d8168b7f0be
1Jamster1 <jasper@live.com.au>
2021-06-12 23:51:06 +1000

    Test Scene updated

---
+++ Objects/Torch/ObjectBase.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var object_collision = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if object_collision == 0:
+		$Animations.play("off")
+	else:
+		$Animations.play("on")
+
+
+func _on_CollisionShape2D_torch_collision():
+	object_collision = 1
+	print("torch start")
+
+
+func _on_CollisionShape2D_torch_collision_end():
+	object_collision = 0
+	print("torch end")
