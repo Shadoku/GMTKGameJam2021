af7eaf7dd8be864e5ff47601098d3b601c57ab7d
Shadoku <Shadoku@gmail.com>
2021-06-12 20:50:08 -0600

    blocks

--- Objects/Block/ObjectBase.gd
+++ Objects/Block/ObjectBase.gd
@@ -1,4 +1,6 @@
 extends Area2D
+var xmov = 0
+var ymov = 0
 
 
 var object_collision = 0
@@ -10,13 +12,48 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Animations.play()
+	#$Animations.play()
+	if xmov > 0:
+		position.x += 1
+		xmov -= 1
+		
+	if xmov < 0:
+		position.x -= 1
+		xmov += 1
+		
+	if ymov > 0:
+		position.y += 1
+		ymov -= 1
+		
+	if ymov < 0:
+		position.y -= 1
+		ymov += 1
+		
 	
 func push(object, direction):
 	if object == self:
-		if direction == "up":
+		if direction == "up" && ymov == 0 && xmov == 0:
+			
 			print("pushed up")
-			position.y -= 16
+			#position.y -= 16
+			ymov = -16
+		if direction == "down" && ymov == 0 && xmov == 0:
+			
+			print("pushed down")
+			#position.y -= 16
+			ymov = 16
+			
+		if direction == "left" && ymov == 0 && xmov == 0:
+			
+			print("pushed left")
+			#position.y -= 16
+			xmov = -16
+			
+		if direction == "right" && ymov == 0 && xmov == 0:
+			
+			print("pushed left")
+			#position.y -= 16
+			xmov = 16
 
 func _on_CollisionShape2D_object_collision():
 	object_collision = 1
